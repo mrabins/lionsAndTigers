e4e26cb986cc9b36572342106c98a1ba9a70f134
mrabins <markrabins@gmail.com>
2014-11-29 13:40:31 -0800

    Change animation added Lion

---
+++ LionsAndTigers/Lion.swift
@@ -0,0 +1,23 @@
+//
+//  Lion.swift
+//  LionsAndTigers
+//
+//  Created by Mark Rabins on 11/25/14.
+//  Copyright (c) 2014 self.edu.swift. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Lion {
+    var age = 0
+    var isAlphaMale = false
+    var image = UIImage(named:"")
+    var name = ""
+    var subspecies = ""
+   
+    func roar(){
+        println("Lion: Roar Roar")
+    }
+    
+}
